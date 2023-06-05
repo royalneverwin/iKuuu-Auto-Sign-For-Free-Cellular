e29da367925ce96d916ddf88bb1be82fc206cb27
royalneverwin <55973858+royalneverwin@users.noreply.github.com>
2023-06-05 20:14:07 +0800

    fix bugs to solve web url changed problem

--- auto_sign_in.py
+++ auto_sign_in.py
@@ -17,17 +17,24 @@ def auto_sign_in(driver, usr, pwd):
 	password = driver.find_element('xpath', '//*[@id="password"]')
 	password.send_keys(pwd)
 
+	# 确认是否打勾 remember me
+	remember_button = driver.find_element('xpath', '//*[@id="remember-me"]')
+	if not remember_button.is_selected:
+		remember_button.click()
+
 	# 使用xpath定位要点击的框并调用click点击
 	login_button = driver.find_element('xpath', '//*[@id="app"]/section/div/div/div/div[2]/form/div/div[5]/button')
 	login_button.click()
 
 	# 跳过弹窗
-	window_read = driver.find_element('xpath', '//*[@id="popup-ann-modal"]/div/div/div[3]/button')
-	window_read.click()
+	window = driver.find_element('xpath', '//*[@id="popup-ann-modal"]/div/div/div[2]/p')
+	if window.is_displayed(): # 有弹窗
+		window_read = driver.find_element('xpath', '//*[@id="popup-ann-modal"]/div/div/div[3]/button')
+		window_read.click()
 
 	# 尝试签到
 	try:
-		daily_sign = driver.find_element('xpath', '//*[@id="checkin-div"]')
+		daily_sign = driver.find_element('xpath', '//*[@id="checkin-div"]/a')
 		print('found')
 		daily_sign.click()
 	except Exception as e:
@@ -40,5 +47,5 @@ def auto_sign_in(driver, usr, pwd):
 if __name__ == '__main__':
 	driver = webdriver.Chrome()
 	driver.implicitly_wait(1)
-	driver.get('https://ikuuu.ltd/auth/login')
+	driver.get('https://ikuuu.eu/auth/login')
 	auto_sign_in(driver, usr, pwd)
